39e067d3deb455bef257c10fdae0b568766082e7
Luca <luca@weve-got-it.com>
2022-08-05 15:17:42 +0200

    Initial Commit

---
+++ Converter_3_Time/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Converter_3_Time
+//
+//  Created by Luca Capriati on 2022/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
